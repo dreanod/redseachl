d706ccf6a9647034556f03463b97c772ceff52f0
Denis Dreano <denis.dreano@gmail.com>
2014-10-27 11:38:26 +0300

    refactored loading chl data

--- scripts/load/loadChl.R
+++ scripts/load/loadChl.R
@@ -1,6 +1,13 @@
 library(R.utils)
 library(raster)
 
+lonmin <- 31
+lonmax <- 45
+latmin <- 10
+latmax <- 31
+crs <- "+proj=longlat +datum=WGS84"
+navalue <- 32767
+dataDir <- "../../data"
 url <- "http://oceandata.sci.gsfc.nasa.gov/cgi/getfile/A20021852002192.L3m_8D_CHL_chlor_a_9km.bz2"
 
 tempZip  <- tempfile()
@@ -8,26 +15,15 @@ tempDest <- tempfile()
 
 download.file(url, tempZip)
 bunzip2(tempZip, tempDest)
-
 myRaster <- raster(tempDest)
 
 unlink(tempZip)
 unlink(tempDest)
 
-plot(myRaster)
-
-proj4string(myRaster) <- CRS("+proj=longlat +datum=WGS84")
+proj4string(myRaster) <- CRS(crs)
 extent(myRaster) <- c(-180, 180, -90, 90)
 
-plot(myRaster)
+redsea <- crop(myRaster, c(lonmin, lonmax, latmin, latmax))
+redsea[redsea == navalue] <- NA
 
-x_min <- 31
-x_max <- 45
-y_min <- 10
-y_max <- 31
-redsea <- crop(myRaster, c(x_min, x_max, y_min, y_max))
-redsea[redsea == 32767] <- NA
-
-
-writeRaster(redsea, "../../data/redseadata.grd")
-
+writeRaster(redsea, paste(dataDir, "redseadata.grd"))

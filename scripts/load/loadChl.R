a2c9bb7bb644d356dd1a0197bee84c24cf5f59b4
Denis Dreano <denis.dreano@gmail.com>
2014-10-28 10:59:35 +0300

    added config file

--- scripts/load/loadChl.R
+++ scripts/load/loadChl.R
@@ -1,20 +1,13 @@
-sessionInfo()
-
 library(R.utils)
 library(raster)
 library(RCurl)
+library(yaml)
 
-lonmin <- 31
-lonmax <- 45
-latmin <- 10
-latmax <- 31
-crs <- "+proj=longlat +datum=WGS84"
-navalue <- 32767
+cst <- yaml.load_file('config.yml')
 dataDir <- "../../data"
-host <- 'http://oceandata.sci.gsfc.nasa.gov'
-path <- 'cgi/getfile'
+year <- 2003
 
-chl_files <- c()
+url_list <- c()
 day_end <- 0
 i <- 0
 while(day_end < 365) {
@@ -22,24 +15,25 @@ while(day_end < 365) {
   day_start <- (i - 1) * 8 + 1
   day_end <- day_start + 7
   if (day_end > 365) day_end <- 365
-  filename <- paste('A2003', sprintf('%03d', day_start), 
-                    '2003', sprintf('%03d', day_end), 
-                    '.L3m_8D_CHL_chlor_a_9km.bz2', sep='')
-  chl_files <- c(chl_files, filename)
+  filename <- paste('A', year, sprintf('%03d', day_start), 
+                    year, sprintf('%03d', day_end), 
+                    cst$chl_file_suffix, sep='')
+  url <- paste(cst$chl_url, filename, sep='/')
+  url_list <- c(url_list, url)
 }
 
 print('Check if files are on the server')
 
-for (f in chl_files) {
-  url <- paste(host, path, f, sep='/')
+for (url in url_list) {
   if(!url.exists(url)) stop('error, non existing file')
 }
 
 print('..............OK')
 
-for (i in 1:length(chl_files)) {
-  f <- chl_files[i]
-  url <- paste(host, path, f, sep='/')
+print('Downloading CHL files')
+
+for (i in 1:length(url_list)) {
+  url <- url_list[i]
   
   tempZip  <- tempfile()
   tempDest <- tempfile()
@@ -48,10 +42,11 @@ for (i in 1:length(chl_files)) {
   bunzip2(tempZip, tempDest)
   myRaster <- raster(tempDest)
   
-  proj4string(myRaster) <- CRS(crs)
+  proj4string(myRaster) <- CRS(cst$crs)
   extent(myRaster) <- c(-180, 180, -90, 90)
   
-  redsea <- crop(myRaster, c(lonmin, lonmax, latmin, latmax))
+  ext <- cst$extent
+  redsea <- crop(myRaster, c(ext$lonmin, ext$lonmax, ext$latmin, ext$latmax))
   redsea[redsea == navalue] <- NA
   
   if (i == 1) {
@@ -63,5 +58,7 @@ for (i in 1:length(chl_files)) {
   unlink(tempZip)
   unlink(tempDest)
 }
+
+print('...................OK')
 
 writeRaster(b, paste(dataDir, "chl.grd", sep="/"))

dcd98acc32de986372f85ffeb518dee9b29af5ad
Denis Dreano <denis.dreano@gmail.com>
2014-12-04 11:03:08 +0300

    download par files / fixed wget flags

--- scripts/load/sst/download.R
+++ scripts/load/sst/download.R
@@ -2,7 +2,7 @@
 URLs <- readLines('scripts/load/sst/files_list.txt')
 
 for (url in URLs) {
-	flags <- '-N --wait=0.5 --random-wait --force-html -i - -P data/sst/raw'
+	flags <- '-N --wait=0.5 --force-html -P data/sst/raw'
 	cmd <- paste('wget', url, flags)
 	system(cmd)
 }

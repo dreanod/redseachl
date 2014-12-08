75edef660e27d4ace1c6843f9eeb914ddbeef2f6
Denis Dreano <denis.dreano@gmail.com>
2014-12-08 16:32:19 +0300

    download subset of ssh data

--- scripts/load/ssh/download.R
+++ scripts/load/ssh/download.R
@@ -3,16 +3,38 @@
 library(yaml)
 
 conf <- yaml.load_file('scripts/load/ssh/config.yml')
-
-host <- 'ftp.aviso.altimetry.fr'
-path <- "/global/delayed-time/grids/msla/all-sat-merged/h"
 password <- conf$password
 login <- conf$login
-url <- paste('ftp://', login, ':', password, '@', host, path, sep='')
+
+lonmin <- 25
+lonmax <- 55
+latmin <- 5
+latmax <- 35
+dirOut <- 'data/ssh/raw'
+
+script <- 'python ~/tools/motu-client-python/motu-client.py'
+f_user <- paste('-u', login)
+f_pass <- paste('-p', password)
+f_url <- '-m http://motu.aviso.oceanobs.com/aviso-gateway-servlet/Motu'
+f_service <- '-s AvisoDT'
+f_dataset <- '-d dataset-duacs-dt-global-allsat-msla-h'
+f_lonmin <- paste('-x', lonmin)
+f_lonmax <- paste('-X', lonmax)
+f_latmin <- paste('-y', latmin)
+f_latmax <- paste('-Y', latmax)
+cmd1 <- paste(script, f_user, f_pass, f_url, f_service, f_dataset, 
+	          f_lonmin, f_lonmax, f_latmin, f_latmax)
+
+f_data <- '-v crs -v lon_bnds -v err -v lat_bnds -v sla -v nv'
+f_dir <- paste('-o', dirOut)
+cmd3 <- paste(f_data, f_dir)
 
 
 for (year in 1993:2013) {
-	flags <- '-r -np -nH --cut-dirs=7 --random-wait --wait 1 -P data/ssh/raw'
-	cmd <- paste('wget', ' ', flags, ' ', url, '/', year, sep='')
+	f_dateBeg <- paste('-t "', year, '-01-01"', sep='')
+	f_dateEnd <- paste('-T "', year, '-12-31"', sep='')
+	f_filename <- paste('-f ', 'sla_', year, '.nc', sep='')
+	cmd <- paste(cmd1, f_dateBeg, f_dateEnd, cmd3, f_filename)
 	system(cmd)
 }
+

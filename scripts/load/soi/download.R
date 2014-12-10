1ec73e8eb97741867ba01c41328479268756a926
Denis Dreano <denis.dreano@gmail.com>
2014-12-10 16:20:29 +0300

    fixed SOI and NAO downloads

--- scripts/load/soi/download.R
+++ scripts/load/soi/download.R
@@ -1,8 +1,3 @@
 
-dataDir <- 'data/soi'
-dir.create(dataDir)
-dataDir <- file.path(dataDir, 'raw')
-dir.create(dataDir)
-
 url <- 'https://www.longpaddock.qld.gov.au/seasonalclimateoutlook/southernoscillationindex/soidatafiles/DailySOI1933-1992Base.txt'
-system(paste('wget', url, '-P', dataDir))
+system(paste('wget', url, '-P', 'data/soi/raw'))

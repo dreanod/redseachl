395134db278f238d142bb1b45e17c1a43199887d
Denis Dreano <denis.dreano@gmail.com>
2015-01-06 14:21:52 +0300

    export figures

--- scripts/exports/chl_ERSEM_ts.R
+++ scripts/exports/chl_ERSEM_ts.R
@@ -38,42 +38,46 @@ sum_reg <- function(df) {
 chl_NRS <- chl[regions == 'NRS',]
 chl_NRS <- sum_reg(chl_NRS)
 write.csv(chl_NRS, file='results/export/chl_ts/chlNRS.csv', row.names=FALSE)
-chl_NRS$reg <- 'NRS'
-chl_NRS <- melt(chl_NRS, id.vars=c('date', 'reg'))
 
 chl_NCRS <- chl[regions == 'NCRS',]
 chl_NCRS <- sum_reg(chl_NCRS)
 write.csv(chl_NCRS, file='results/export/chl_ts/chlNCRS.csv', row.names=FALSE)
-chl_NCRS$reg <- 'NCRS'
-chl_NCRS <- melt(chl_NCRS, id.vars=c('date', 'reg'))
 
 chl_SCRS <- chl[regions == 'SCRS',]
 chl_SCRS <- sum_reg(chl_SCRS)
 write.csv(chl_SCRS, file='results/export/chl_ts/chlSCRS.csv', row.names=FALSE)
-chl_SCRS$reg <- 'SCRS'
-chl_SCRS <- melt(chl_SCRS, id.vars=c('date', 'reg'))
 
 chl_SRS <- chl[regions == 'SRS',]
 chl_SRS <- sum_reg(chl_SRS)
 write.csv(chl_SRS, file='results/export/chl_ts/chlSRS.csv', row.names=FALSE)
-chl_SRS$reg <- 'SRS'
-chl_SRS <- melt(chl_SRS, id.vars=c('date', 'reg'))
 
 ## Generate figures about summaries
-
+chl_NRS$reg <- 'NRS'
+chl_NRS <- melt(chl_NRS, id.vars=c('date', 'reg'))
+chl_NCRS$reg <- 'NCRS'
+chl_NCRS <- melt(chl_NCRS, id.vars=c('date', 'reg'))
+chl_SCRS$reg <- 'SCRS'
+chl_SCRS <- melt(chl_SCRS, id.vars=c('date', 'reg'))
+chl_SRS$reg <- 'SRS'
+chl_SRS <- melt(chl_SRS, id.vars=c('date', 'reg'))
 chl <- rbind(chl_NRS, chl_NCRS, chl_SCRS, chl_SRS)
 
 p <- ggplot(subset(chl, variable=='mean'), aes(as.Date(date), value, group=reg))
 p <- p + geom_line(linewidth=.001)
 p <- p + facet_grid(reg ~ .)
-p
+p <- p + ggtitle('logCHL mean for each region')
+ggsave('results/export/chl_ts/mean.png')
 
 p <- ggplot(subset(chl, variable=='std'), aes(as.Date(date), value, group=reg))
 p <- p + geom_line(linewidth=.001)
 p <- p + facet_grid(reg ~ .)
-p
+p <- p + ggtitle('standard deviation of logCHL mean')
+ggsave('results/export/chl_ts/std.png')
 
 p <- ggplot(subset(chl, variable=='mis'), aes(as.Date(date), value, group=reg))
 p <- p + geom_line(linewidth=.001)
 p <- p + facet_grid(reg ~ .)
-p
+p <- p + ggtitle('ratio of missing values for each region')
+ggsave('results/export/chl_ts/mis.png')
+
+## Generate climatological data

dcb38aa0fa4870af8c1af8236536168dfceede6b
Denis Dreano <denis.dreano@gmail.com>
2015-01-06 14:08:19 +0300

    exported region summaries to csv

--- scripts/exports/chl_ERSEM_ts.R
+++ scripts/exports/chl_ERSEM_ts.R
@@ -37,23 +37,29 @@ sum_reg <- function(df) {
 
 chl_NRS <- chl[regions == 'NRS',]
 chl_NRS <- sum_reg(chl_NRS)
+write.csv(chl_NRS, file='results/export/chl_ts/chlNRS.csv', row.names=FALSE)
 chl_NRS$reg <- 'NRS'
 chl_NRS <- melt(chl_NRS, id.vars=c('date', 'reg'))
 
 chl_NCRS <- chl[regions == 'NCRS',]
 chl_NCRS <- sum_reg(chl_NCRS)
+write.csv(chl_NCRS, file='results/export/chl_ts/chlNCRS.csv', row.names=FALSE)
 chl_NCRS$reg <- 'NCRS'
 chl_NCRS <- melt(chl_NCRS, id.vars=c('date', 'reg'))
 
 chl_SCRS <- chl[regions == 'SCRS',]
 chl_SCRS <- sum_reg(chl_SCRS)
+write.csv(chl_SCRS, file='results/export/chl_ts/chlSCRS.csv', row.names=FALSE)
 chl_SCRS$reg <- 'SCRS'
 chl_SCRS <- melt(chl_SCRS, id.vars=c('date', 'reg'))
 
 chl_SRS <- chl[regions == 'SRS',]
 chl_SRS <- sum_reg(chl_SRS)
+write.csv(chl_SRS, file='results/export/chl_ts/chlSRS.csv', row.names=FALSE)
 chl_SRS$reg <- 'SRS'
 chl_SRS <- melt(chl_SRS, id.vars=c('date', 'reg'))
+
+## Generate figures about summaries
 
 chl <- rbind(chl_NRS, chl_NCRS, chl_SCRS, chl_SRS)
 
@@ -71,9 +77,3 @@ p <- ggplot(subset(chl, variable=='mis'), aes(as.Date(date), value, group=reg))
 p <- p + geom_line(linewidth=.001)
 p <- p + facet_grid(reg ~ .)
 p
-# dir.create('results/export')
-# dir.create('results/export/chl_ts')
-# save(chl_NRS, chl_NCRS, chl_SCRS, chl_SRS, 
-#      file='results/export/chl_ts/diosReg.Rda')
-
-## Time series according to clustering

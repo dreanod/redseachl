fbe06b4e01663aa86645505ff0711a3dd7437b48
Denis Dreano <denis.dreano@gmail.com>
2015-01-06 13:01:42 +0300

    summarize all regions

--- scripts/exports/chl_ERSEM_ts.R
+++ scripts/exports/chl_ERSEM_ts.R
@@ -1,7 +1,7 @@
 load('data/chl/formated/chlCast.Rda')
 
 chl <- chl_df
-dates <- colnames(chl_df)
+dates <- colnames(chl_df)[3:length(dates)]
 chl <- data.matrix(chl)
 lon <- chl[,1]
 lat <- chl[,2]
@@ -33,48 +33,17 @@ sum_reg <- function(df) {
   return(data.frame(date=dates, mean=df_mean, std=df_std, mis=df_mis))
 }
 
-chl_NRS  <- chl[regions == 'NRS',]
+chl_NRS <- chl[regions == 'NRS',]
 chl_NRS <- sum_reg(chl_NRS)
 
 chl_NCRS <- chl[regions == 'NCRS',]
-chl_mean <- colMeans(chl_NCRS, na.rm=TRUE)
-chl_std  <- apply(chl_NCRS, 2, sd, na.rm =TRUE)
-chl_mis  <- apply(chl_NCRS, 2, rat_NA)
-chl_NCRS <- data.frame(date=dates, mean=chl_mean, std=chl_std, mis=chl_mis)
+chl_NCRS <- sum_reg(chl_NCRS)
 
 chl_SCRS <- chl[regions == 'SCRS',]
-chl_mean <- colMeans(chl_NRS, na.rm=TRUE)
-chl_std  <- apply(chl_NRS, 2, sd, na.rm =TRUE)
-chl_mis  <- apply(chl_NRS, 2, rat_NA)
-chl_NRS <- data.frame(date=date, mean=chl_mean, std=chl_std, mis=chl_mis)
+chl_SCRS <- sum_reg(chl_SCRS)
 
-chl_SRS  <- chl[regions == 'SRS',]
-chl_mean <- colMeans(chl_NRS, na.rm=TRUE)
-chl_std  <- apply(chl_NRS, 2, sd, na.rm =TRUE)
-chl_mis  <- apply(chl_NRS, 2, rat_NA)
-chl_NRS <- data.frame(date=date, mean=chl_mean, std=chl_std, mis=chl_mis)
-
-aggregate_chl <- function(chl_df) {
-  chl_agg = NULL
-  chl_percNA = NULL
-  chl_std = NULL
-  
-  for (d in dates) {
-    chl_d <- subset(chl_df, date == d)
-    chl_agg <- c(chl_agg, mean(chl_d$chl, na.rm=TRUE))
-    chl_std <- c(chl_std, sd(chl_d$chl, na.rm=TRUE))
-    chl_percNA <- c(chl_percNA, sum(is.na(chl_d$chl))/length(is.na(chl_d$chl)))
-  }
-  
-  aggregate <- data.frame(dates=dates, logChl=chl_agg, std=chl_std, 
-                          percNA=chl_percNA)
-  return(aggregate)
-}
-
-chl_NRS <- aggregate_chl(chl_NRS)
-chl_NCRS <- aggregate_chl(chl_NCRS)
-chl_SCRS <- aggregate_chl(chl_SCRS)
-chl_SRS <- aggregate_chl(chl_SRS)
+chl_SRS <- chl[regions == 'SRS',]
+chl_SRS <- sum_reg(chl_SRS)
 
 dir.create('results/export')
 dir.create('results/export/chl_ts')

3273c949ba0e52b8612e3c984eb60921aec2f9b6
Denis Dreano <denis.dreano@gmail.com>
2015-01-12 17:45:17 +0300

    script to aggregate/crop sst raster

--- scripts/load/sst/aggregate.sh
+++ scripts/load/sst/aggregate.sh
@@ -3,4 +3,13 @@
 mkdir data/sst/temp
 mkdir data/sst/aggregate
 
-for f in data/sst/raw/*.bz2; do:
+for f in data/sst/raw/*.bz2; do
+  filename=$(basename "$f")
+  filename=data/sst/temp/"${filename%.*}"
+  bzcat $f > $filename
+  
+  gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 180 -90 \
+  HDF4_SDS:UNKNOWN:\"$filename\":0 $filename
+
+  gdal_translate -projwin 25 35 55 5 $filename $filename
+done

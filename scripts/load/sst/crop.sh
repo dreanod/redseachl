2944152583e34e7c6b2c1964d9f0604a0e5a285b
Denis Dreano <denis.dreano@gmail.com>
2015-01-15 13:56:17 +0300

    shell script to crop and convert sst data

--- scripts/load/sst/crop.sh
+++ scripts/load/sst/crop.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 mkdir data/sst/temp
-mkdir data/sst/aggregate
+mkdir data/sst/crop
 
 for f in data/sst/raw/*.bz2; do
   filename=$(basename "$f")
   f_temp=data/sst/temp/"${filename%.*}"
-  f_fin=data/sst/aggregate/"${filename%.*}"
+  f_fin=data/sst/crop/"${filename%.*}".tiff
   bzcat $f > $f_temp
   
   gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 180 -90 \
   HDF4_SDS:UNKNOWN:\"$f_temp\":0 $f_temp
+  
+  gdal_translate -projwin 25 35 65 5 $f_temp $f_fin
 
-  gdal_translate -projwin 25 35 55 5 $f_temp $f_fin
-
-  rm $_temp
+  rm $f_temp
 done
 
 rm -r data/sst/temp

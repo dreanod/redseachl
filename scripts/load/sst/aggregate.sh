8503948a9b8c41f1d4747be63fb26bf78a416ffa
Denis Dreano <denis.dreano@gmail.com>
2015-01-15 09:11:09 +0300

    cropping data

--- scripts/load/sst/aggregate.sh
+++ scripts/load/sst/aggregate.sh
@@ -5,11 +5,16 @@ mkdir data/sst/aggregate
 
 for f in data/sst/raw/*.bz2; do
   filename=$(basename "$f")
-  filename=data/sst/temp/"${filename%.*}"
-  bzcat $f > $filename
+  f_temp=data/sst/temp/"${filename%.*}"
+  f_fin=data/sst/aggregate/"${filename%.*}"
+  bzcat $f > $f_temp
   
   gdal_translate -a_srs EPSG:4326 -a_ullr -180 90 180 -90 \
-  HDF4_SDS:UNKNOWN:\"$filename\":0 $filename
+  HDF4_SDS:UNKNOWN:\"$f_temp\":0 $f_temp
 
-  gdal_translate -projwin 25 35 55 5 $filename $filename
+  gdal_translate -projwin 25 35 55 5 $f_temp $f_fin
+
+  rm $_temp
 done
+
+rm -r data/sst/temp

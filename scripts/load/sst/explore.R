056bc3ae04c32e388b833865ee604e3686f963db
Denis Dreano <denis.dreano@gmail.com>
2015-01-18 13:56:49 +0300

    histogram SST per year

--- scripts/load/sst/explore.R
+++ scripts/load/sst/explore.R
@@ -1,14 +1,20 @@
 library(raster)
- 
-FILES <- list.files('data/sst/clean', full.names=TRUE, pattern='*.grd')
-values = vector()
+library(ggplot2)
 
-for (f in FILES) {
-  print(f)
+dir.create('derived/EDA/SST/SST_around_Arabia', recursive=TRUE)
 
-  r <- raster(f)
+for (y in 2002:2014) {
+  FILES <- list.files('data/sst/clean', full.names=TRUE, 
+                      pattern=paste('A', y ,'.*.grd', sep=''))
+  values = vector()
 
-  values = c(values, as.vector(r))
+  for (f in FILES) {
+    print(f)
+    r <- raster(f)
+    values = c(values, as.vector(r))
+  }
+  p <- qplot(values) + xlab('SST in Celsius') 
+  p <- p + ggtitle(paste('SST around Arabia in', y))
+  filename <- paste('derived/EDA/SST/SST_around_arabia/', y, '.png', sep='')
+  ggsave(filename=filename, plot = p)
 }
-
-hist(values)

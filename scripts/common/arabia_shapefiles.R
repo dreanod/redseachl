c33e481d8c484dc951378676e43f2acf0475c4f2
Denis Dreano <denis.dreano@gmail.com>
2015-01-18 15:19:29 +0300

    shapefiles countries

--- scripts/common/arabia_shapefiles.R
+++ scripts/common/arabia_shapefiles.R
@@ -1,16 +1,22 @@
+library(raster)
 library(maptools)
 library(PBSmapping)
 library(R.utils)
+library(yaml)
+library(rgeos)
 
-data_dir <- 'data/common'
-dir.create(data_dir)
+config <- yaml.load_file('scripts/load/config.yml')
+extent <- config$large_extent
+
+data_dir <- 'data/common/shapefiles'
+dir.create(data_dir, recursive=TRUE)
 
 crs <- CRS("+proj=longlat +datum=WGS84")
 
-x_min <- 31
-x_max <- 45
-y_min <- 10
-y_max <- 31
+x_min <- extent$lon_min
+x_max <- extent$lon_max
+y_min <- extent$lat_min
+y_max <- extent$lat_max
 
 bbox <- matrix(c(x_min,x_max,x_max,x_min,x_min,
                  y_min,y_min,y_max,y_max,y_min),
@@ -29,11 +35,8 @@ red.sea.countries <- SpatialPolygons2PolySet(red.sea.countries)
 red.sea.countries <- clipPolys(red.sea.countries, xlim = c(x_min, x_max), ylim = c(y_min, y_max))
 red.sea.outside <- PolySet2SpatialPolygons(red.sea.countries, close_polys=TRUE)
 projection(red.sea.outside) <- crs
-red.sea.inside  <- gDifference(bbox, red.sea.outside)
 
 plot(red.sea.outside)
-plot(red.sea.inside)
 
 save(red.sea.outside, file=paste(data_dir, "red_sea_outside.Rdata", sep="/"))
-save(red.sea.inside,  file=paste(data_dir, "red_sea_inside.Rdata",  sep="/"))
 

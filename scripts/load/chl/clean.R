b5576266f21e39ef2bb5fa2bd2cc4c7e4443bca8
Denis Dreano <denis.dreano@gmail.com>
2015-01-19 17:15:00 +0300

    clean chl data

---
+++ scripts/load/chl/clean.R
@@ -0,0 +1,14 @@
+library(raster)
+
+dir.create('data/chl/clean')
+
+FILES <- list.files('data/chl/crop', full.names=TRUE)
+
+for (f in FILES) {
+  print(f)
+
+  r <- raster(f)
+  r[r == -32767] = NA # masking missing values
+
+  writeRaster(r, paste('data/chl/clean', basename(f), '.grd', sep=''))
+}

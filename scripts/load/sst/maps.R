304a81e87dc2d48901138561b3c0c5e5cb6f4020
Denis Dreano <denis.dreano@gmail.com>
2015-01-19 10:24:06 +0300

    function to extract date from filename

--- scripts/load/sst/maps.R
+++ scripts/load/sst/maps.R
@@ -1,12 +1,24 @@
 library(rasterVis)
 library(ggplot2)
 
+date_from_filename <- function(f) {
+  b <- basename(f)
+  b <- splitstr(b, sep='.')[[1]]
+  b <- b[1]
+
+  y <- substring(b, 2, 5)
+  d <- substring(b, 6, 8)
+
+  t <- as.Date(paste('01-01-', y, sep='')) + d - 1
+  return(t)
+}
+
 dir.create('derived/EDA/SST/weekly_maps/', recursive=TRUE)
 load('data/common/shapefiles/red_sea_outside.Rdata')
 
 FILE <- list.files('data/sst/clean', full.names=TRUE)
 
-for (f in FILE) {
+for (f in FILE[1:2]) {
   r <- raster(f)
   df <- as.data.frame(r, xy=TRUE)
   names(df) <- c('long', 'lat', 'layer')
@@ -16,7 +28,8 @@ for (f in FILE) {
                                             limits=c(15, 35))
   p <- p + coord_cartesian() 
   p <- p + geom_polygon(aes(x=long, y=lat, group=group), red.sea.outside,
-                        fill='white', contour='tan', size=5)
+                        fill='white', contour='black', size=50)
+  p <- p + legend('SST')
 
   ggsave(filename=paste('derived/EDA/SST/weekly_maps/', basename(f), '.png', sep=''), plot=p)
 }

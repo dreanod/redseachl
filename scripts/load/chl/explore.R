54b1ace2fc03129676501fbd36e65b85bb35098e
Denis Dreano <denis.dreano@gmail.com>
2015-01-19 17:26:38 +0300

    generate chl histograms

---
+++ scripts/load/chl/explore.R
@@ -0,0 +1,22 @@
+library(raster)
+library(ggplot2)
+
+export_dir <- 'derived/EDA/CHL/hist_chl_arabia'
+dir.create(export_dir, recursive=TRUE)
+
+for(y in 2002:2014) {
+  FILES <- list.files('data/chl/clean', full.names=TRUE,
+                      pattern=paste('A', y, '.*.grd', sep=''))
+  values = vector()
+
+  for (f in FILES) {
+    print(f)
+    r <- raster(f)
+    values <- c(values, as.vector(r))
+  }
+  p <- qplot(values) + xlab('CHL in mg/m^3')
+  p <- p + ggtitle(paste('chl around Arabia in', y))
+  filename <- paste(export_dir, y, '.png', sep='')
+
+  ggsave(filename=filename, plot=p)
+}

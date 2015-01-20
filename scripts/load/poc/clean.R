37152cb61575e3c47ff558d69ae431155bdad1bb
Denis Dreano <denis.dreano@gmail.com>
2015-01-20 11:29:33 +0300

    cleaned POC data

---
+++ scripts/load/poc/clean.R
@@ -0,0 +1,14 @@
+library(raster)
+
+dir.create('data/poc/clean')
+
+FILES <- list.files('data/poc/crop', full.names=TRUE)
+
+for (f in FILES) {
+  print(f)
+
+  r <- raster(f)
+  r[r == -32767] = NA # masking missing values
+
+  writeRaster(r, paste('data/poc/clean/', basename(f), '.grd', sep=''))
+}

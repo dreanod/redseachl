ac18d17a782e3cecb64725b49b2ca9c8983a557d
Denis Dreano <denis.dreano@gmail.com>
2015-03-03 16:41:59 +0300

    last corrections from Ibrahim

--- scripts/proposal/plotChlEcoModels.R
+++ scripts/proposal/plotChlEcoModels.R
@@ -22,13 +22,14 @@ df_obs <- data.frame(date=modis_dates, chl=modis)
 
 p <- ggplot() + geom_line(aes(x=date, y=chl_assim, color='Assimilated'), df)
 p <- p + geom_line(aes(x=date, y=chl_free, color='Free run'), df, ylim=c(0, 0.5))
-p <- p + geom_line(aes(x=date, y=chl, color='Observations'), df_obs)
+p <- p + geom_point(aes(x=date, y=chl, color='Observations'), df_obs)
 p <- p + scale_colour_manual(name='', values=c('Assimilated'='red',
                              'Free run'='blue', 'Observations'='green'))
 p <- p + ylab('chl concentration in mg/m3')
 p <- p + coord_cartesian(ylim=c(0, 0.5))
 p <- p + ggtitle('Central Red Sea')
-ggsave('results/proposal/figures/chl_models1.png', p)
+ggsave('results/proposal/figures/chl_models1.png', p, units='cm', height=16,
+       width=28)
 
 ###############################################################
 
@@ -49,13 +50,14 @@ df_obs <- data.frame(date=modis_dates, chl=modis)
 
 p <- ggplot() + geom_line(aes(x=date, y=chl_assim, color='Assimilated'), df)
 p <- p + geom_line(aes(x=date, y=chl_free, color='Free run'), df, ylim=c(0, 0.5))
-p <- p + geom_line(aes(x=date, y=chl, color='Observations'), df_obs)
+p <- p + geom_point(aes(x=date, y=chl, color='Observations'), df_obs)
 p <- p + scale_colour_manual(name='', values=c('Assimilated'='red',
                              'Free run'='blue', 'Observations'='green'))
 p <- p + ylab('chl concentration in mg/m3')
 #p <- p + coord_cartesian(ylim=c(0, 0.5))
 p <- p + ggtitle('Northern Red Sea')
-ggsave('results/proposal/figures/chl_models2.png', p)
+ggsave('results/proposal/figures/chl_models2.png', p, units='cm', height=16,
+       width=28)
 
 ###############################################################
 
@@ -76,11 +78,12 @@ df_obs <- data.frame(date=modis_dates, chl=modis)
 
 p <- ggplot() + geom_line(aes(x=date, y=chl_assim, color='Assimilated'), df)
 p <- p + geom_line(aes(x=date, y=chl_free, color='Free run'), df, ylim=c(0, 0.5))
-p <- p + geom_line(aes(x=date, y=chl, color='Observations'), df_obs)
+p <- p + geom_point(aes(x=date, y=chl, color='Observations'), df_obs)
 p <- p + scale_colour_manual(name='', values=c('Assimilated'='red',
                              'Free run'='blue', 'Observations'='green'))
 p <- p + ylab('chl concentration in mg/m3')
 #p <- p + coord_cartesian(ylim=c(0, 0.5))
 p <- p + ggtitle('Southern Red Sea')
 p <- p + coord_cartesian(ylim=c(0, 0.85))
-ggsave('results/proposal/figures/chl_models3.png', p)
+ggsave('results/proposal/figures/chl_models3.png', p, units='cm', height=16,
+       width=28)
